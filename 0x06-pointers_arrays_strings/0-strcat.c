7d48fdcc1292f8fb5a8a8537555c8c3db88b0790
Junaid Valentyn <junaidodile@gmail.com>
2022-06-16 15:20:14 -0700

    Task 0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,8 +8,7 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
+int i, j;
 
 i = 0;
 while (dest[i] != '\0')
@@ -24,7 +23,6 @@ dest[i] = src[j];
 j++;
 i++;
 }
-
 dest[i] = '\0';
 
 return (dest);

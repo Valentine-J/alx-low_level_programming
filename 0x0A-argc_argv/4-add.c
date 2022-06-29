9cf2522044dd5d372dc0e8a9afa48d0cdc88e855
Junaid Valentyn <junaidodile@gmail.com>
2022-06-29 09:49:09 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,38 +4,69 @@
 #include <string.h>
 
 /**
- * main - Print the positive numbers, followed by a new line
- * @argc: Number of arguments
- * @argv: Array of arguments
- * Return: Always 0 (Success), 1 (failure)
+ * check_num - check - string there are digit
+ * @str: array str
+ * Return: Always 0 (Success)
+ */
+
+int check_num(char *str)
+{
+
+/*Declaring variables*/
+unsigned int count;
+
+count = 0;
+
+while (count < strlen(str)) /*count string*/
+{
+if (!isdigit(str[count])) /*check if str there are digit*/
+{
+return (0);
+}
+
+count++;
+}
+
+return (1);
+}
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ * return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
 
-int i;
-int j;
+/*Declaring variables*/
+int count;
+int str_to_int;
 int sum = 0;
 
-if (argc == 1)
+count = 1;
+
+while (count < argc) /*Goes through the whole array*/
 {
-printf("%d\n", argc - 1);
-return (0);
+if(check_num(argv[count]))
+{
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+sum += str_to_int;
 }
 
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0', j++)
-if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
+/*Condition if one of the number contains symbols that are not digits*/
+else
 {
 printf("Error\n");
+
 return (1);
 }
 
-sum += atoi(argv[i]);
+count++;
 }
 
-printf("%d\n", sum);
+printf("%d\n", sum); /*print sum*/
 
 return (0);
 }

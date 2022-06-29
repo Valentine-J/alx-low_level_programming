671538290213faf9496b09942434325af63aabb9
Junaid Valentyn <junaidodile@gmail.com>
2022-06-29 09:19:12 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 /**
- * main - Print the positive numbers
+ * main - Print the positive numbers, followed by a new line
  * @argc: Number of arguments
  * @argv: Array of arguments
  * Return: Always 0 (Success), 1 (failure)
@@ -15,18 +15,19 @@ int i;
 int j;
 int sum = 0;
 
-count = 1;
+if (argc == 1)
+{
+printf("%d\n", argc - 1);
+return (0);
+}
 
 for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0', j++)
-{
-if (argv[i][j] < '0' || argv[i][j] > '9')
+if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
 {
 printf("Error\n");
 return (1);
-}
-
 }
 
 sum += atoi(argv[i]);
